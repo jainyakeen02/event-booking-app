f55729d4048cf0139f9c9f6951fa0b5cf7a880e3
ridham077 <ridham2212006@gmail.com>
2026-04-06 10:57:33 +0530

    Create Booking.js

---
+++ backend/models/Booking.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    event: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Event",
+      required: true,
+    },
+    tickets: {
+      type: Number,
+      required: true,
+    },
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Booking = mongoose.model("Booking", bookingSchema);
+
+export default Booking;
