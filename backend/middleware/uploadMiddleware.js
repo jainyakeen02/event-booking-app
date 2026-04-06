1eedb5a8502c1be4f600b81c3b1adee706c4e635
ridham077 <ridham2212006@gmail.com>
2026-04-06 10:55:59 +0530

    Create uploadMiddleware.js

---
+++ backend/middleware/uploadMiddleware.js
@@ -0,0 +1,39 @@
+import multer from "multer";
+import path from "path";
+import fs from "fs";
+
+const uploadPath = "uploads";
+
+// uploads folder automatic create ho jayega
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath);
+}
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadPath);
+  },
+  filename: function (req, file, cb) {
+    const uniqueName = Date.now() + "-" + file.originalname;
+    cb(null, uniqueName);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = /jpg|jpeg|png|webp/;
+  const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = allowedTypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only image files are allowed"));
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter,
+});
+
+export default upload;
