e77c696defd1c8cacf0fc5f9140541afd6e4821a
ridham077 <ridham2212006@gmail.com>
2026-04-06 10:58:04 +0530

    Create Event.js

---
+++ backend/models/Event.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+const eventSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    date: {
+      type: Date,
+      required: true,
+    },
+
+    location: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+    },
+
+    seats: {
+      type: Number,
+      required: true,
+    },
+
+    bookedSeats: {
+      type: Number,
+      default: 0,
+    },
+
+    image: {
+      type: String,
+    },
+
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const Event = mongoose.model("Event", eventSchema);
+
+export default Event;
